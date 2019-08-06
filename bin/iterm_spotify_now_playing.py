f0a9fb06a977589bd97548a48da03436216b71be
phixyn <phixyn@gmail.com>
2019-08-06 21:32:05 +0100

    :sparkles: add text for different player states (e.g. paused)
    Updates the status bar text to display different icons and text
    when the Spotify player is stopped or paused. Also removes
    unnecessary globals.

--- bin/iterm_spotify_now_playing.py
+++ bin/iterm_spotify_now_playing.py
@@ -4,28 +4,30 @@ in a status bar component.
 
 
 __author__ = "Phixyn (Alpeche Pancha)"
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 
 
 import iterm2
 import subprocess
 
 
-song = ""
-artist = ""
-temp_song = ""
-
 get_song_cmd = [
     "osascript",
     "-e",
     'tell application "Spotify" to name of current track as string'
 ]
-
 get_artist_cmd = [
     "osascript",
     "-e",
     'tell application "Spotify" to artist of current track as string'
 ]
+get_spotify_state = [
+    "osascript",
+    "-e",
+    'tell application "Spotify" to player state'
+]
+
+temp_song = ""
 
 
 async def main(connection):
@@ -46,10 +48,6 @@ async def main(connection):
         except:
             return
 
-        global song
-        global artist
-        global temp_song
-
         try:
             song = subprocess.run(
                 get_song_cmd,
@@ -57,7 +55,6 @@ async def main(connection):
                 capture_output=True,
                 timeout=5,
                 text=True).stdout.strip()
-
             artist = subprocess.run(
                 get_artist_cmd,
                 check=True,
@@ -70,13 +67,27 @@ async def main(connection):
             print("Could not get song name or artist from Spotify process.")
             print(ex)
 
+        try:
+            state = subprocess.run(
+                get_spotify_state,
+                check=True,
+                capture_output=True,
+                timeout=5,
+                text=True).stdout.strip()
+        except subprocess.CalledProcessError as ex:
+            state = "stopped"
+            print("Could not get player state from Spotify process. Defaulting to stopped.")
+            print(ex)
+
         # Individual sessions may have a stale song name. We need to keep track of the
         # current song ourselves.
+        global temp_song
         if song != temp_song:
             temp_song = song
 
         await session.async_set_variable("user.currentSong", song)
         await session.async_set_variable("user.currentArtist", artist)
+        await session.async_set_variable("user.currentState", state)
 
     # This function gets called whenever any of the paths named in defaults (below) changes
     # or its configuration changes.
@@ -87,17 +98,23 @@ async def main(connection):
     async def coro(
         knobs,
         current_song=iterm2.Reference("user.currentSong?"),
-        current_artist=iterm2.Reference("user.currentArtist?")
+        current_artist=iterm2.Reference("user.currentArtist?"),
+        current_state=iterm2.Reference("user.currentState?")
     ):
-        if not current_artist and not current_song:
-            return "■ No track playing"
-        return "▶ {} - {}".format(current_artist, current_song)
+        if (not current_artist and not current_song) or \
+            (current_state == "stopped" or not current_state):
+            return "■ Not playing"
+        elif current_state == "paused":
+            # return "❚ ❚ {} - {}".format(current_artist, current_song)
+            return "❙ ❙ {} - {}".format(current_artist, current_song)
+        else:
+            return "▶ {} - {}".format(current_artist, current_song)
 
     # Register the status bar component.
     await component.async_register(connection, coro)
 
-    # Monitor Spotify
     while True:
+        # Monitor Spotify
         await poll_spotify()
 
 iterm2.run_forever(main)
