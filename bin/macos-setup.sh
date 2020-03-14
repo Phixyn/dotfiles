5b127d8ee6765bd56428bbac2d67bc338b2c2bdf
Phixyn <phixyn@gmail.com>
2020-03-14 20:52:59 +0000

    :package: add a new setup script for MacOS
    Add a script that quickly sets up a new MacOS installation by using Homebrew
    and copying relevant dotfiles to their respective folders.

---
+++ bin/macos-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Set up bash, vim and applications on MacOS.
+# NOTE: Run from root directory of dotfiles, not bin folder or anywhere else!
+
+if [[ "$(basename $PWD)" != "dotfiles" ]]; then
+  echo "[ERROR] Please run from root directory of dotfiles, not bin or anywhere else."
+  exit 1
+fi
+
+echo "[INFO] Installing Homebrew..."
+echo "[WARN] This uses sudo, get ready to type password multiple times."
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+#brew tap Homebrew/bundle
+echo "[INFO] Installing applications from Brewfile..."
+echo "[WARN] Get ready to type password."
+brew bundle
+
+echo "[INFO] Downloading git-completion.bash and git-prompt.sh..."
+curl -o $HOME/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o $HOME/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+echo "[INFO] Copying bin files to $HOME/bin..."
+cp -v bin/* $HOME/bin/
+
+echo "[INFO] Copying .bash_profile to user folder."
+cp .bash_profile $HOME/.bash_profile
+
+echo "[INFO] Executing vim-setup.sh script..."
+/bin/bash ./bin/vim-setup.sh
+
+echo "[INFO] MacOS setup done."
